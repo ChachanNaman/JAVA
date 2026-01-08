ba31c52addcca862bddff5ce6ed162b424884bc8
Naman Chachan <143930974+ChachanNaman@users.noreply.github.com>
2026-01-08 22:22:12 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,12 @@
+// To run the java code , you must have JVM on your device 
+// JVM(Java Virtual Machine) runs on top OS and Hardware 
+// JVM only understand only ByteCode 
+// Java code(.java) -> Compiler(JavaC) -> ByteCode(.class) -> JVM -> File must
+// JVM is inside of JRE(Java Runtime Environment)
+// Layers : Hardware -> OS -> JRE -> JVM -> ByteCode runs
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Naman");
+    }
+}
---
+++ Variables2.java
@@ -0,0 +1,8 @@
+public class Variables2{
+    public static void main(String a[]){
+        int num1 = 33;
+        int num2 = 22;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
