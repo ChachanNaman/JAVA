250feff4bef7db4d7d0a00cca158048e8e7fdb37
Naman Chachan <143930974+ChachanNaman@users.noreply.github.com>
2026-01-10 13:49:30 +0530

    started oops

--- 3ControlFlow/ForLoop7.java
+++ 3ControlFlow/ForLoop7.java
@@ -1,6 +1,8 @@
 public class ForLoop7{
 
     public static void main(String a[]){
-        
+        for(int i = 1; i <= 4; i++){
+            System.out.println("Hello" + i );
+        }
     }
 }
