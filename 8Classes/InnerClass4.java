549279127907b19f39d844853036d9855c075449
Naman Chachan <143930974+ChachanNaman@users.noreply.github.com>
2026-01-17 19:32:59 +0530

    pushed final

---
+++ 8Classes/InnerClass4.java
@@ -0,0 +1,25 @@
+class A{
+    public void show(){
+        System.out.println("In A class");
+    }
+
+    class B{  
+    // we can make innerclass static also -> static class B{} and in main i have to call like 
+    //  A.B obj1 = new A.B();
+        public void show1(){
+            System.out.println("In B class");
+        }
+    }
+}
+
+
+public class InnerClass4 {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        //now as if B is subclass of A class then
+        A.B obj1 = obj.new B();  //must be under obj 
+        obj1.show1();
+    }
+}
