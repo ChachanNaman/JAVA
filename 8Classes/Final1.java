0280def9986680e06df7320874b2fd3eba7f6d29
Naman Chachan <143930974+ChachanNaman@users.noreply.github.com>
2026-01-20 00:01:18 +0530

    final

--- 8Classes/Final1.java
+++ 8Classes/Final1.java
@@ -9,7 +9,7 @@ class Calc{
 //Final -> variable : if i want to make variable constant -> final int marks = 8;
 //          class : if i dont want to inherit my class  -> final class Calc
 //          method : if i dont want to override one function in my class (to prevent overriding ) -> public final void show(){}
-
+//finalll
 class AdvCalc extends Calc {
     public void show(){ 
         System.out.println("Naman");
