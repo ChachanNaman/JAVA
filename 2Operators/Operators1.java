6cce0f0b06c7d10ed3f263564ad14f29c41ffcc7
Naman Chachan <143930974+ChachanNaman@users.noreply.github.com>
2026-02-22 00:00:08 +0530

    Update Operators1.java

--- 2Operators/Operators1.java
+++ 2Operators/Operators1.java
@@ -2,7 +2,7 @@ public class Operators1{
 
     public static void main(String j[]){
 
-        //Operators : uujnnnejeje
+        //Operators : uujnnnejejejjjj
 
             int num = 7;
             
